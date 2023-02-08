24836583b51a19efec997f95abe4daaf2e147e71
Victor <victorp2908@gmail.com>
2023-02-08 23:05:06 +0300

    main.py, .gitignore

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-# Документация 
-# Detection Classes: https://imageai.readthedocs.io/en/latest/detection/index.html
-# Video and Live-Feed Detection and Analysis: https://imageai.readthedocs.io/en/latest/video/index.html
 from imageai.Detection import ObjectDetection
 import os
 
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path , "yolov3.pt"))
+# detector.setModelTypeAsYOLOv3()
+# detector.setModelPath( os.path.join(execution_path , "models/yolov3.pt"))
+# detector.setModelTypeAsTinyYOLOv3()
+# detector.setModelPath( os.path.join(execution_path , "models/tiny-yolov3.pt"))
+detector.setModelTypeAsRetinaNet()  # Самая крупная модель
+detector.setModelPath( os.path.join(execution_path , "models/retinanet_resnet50_fpn_coco-eeacb38b.pth"))
 detector.loadModel()
-# detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"), minimum_percentage_probability=30)
 
 detections = detector.detectObjectsFromImage(input_image=r"image.jpg", output_image_path=r"imagenew.jpg")
 # detections = detector.detectObjectsFromImage(input_image=r"image.jpg", output_image_path=r"imagenew.jpg", minimum_percentage_probability=50)
+# detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"), minimum_percentage_probability=30)
 
 for eachObject in detections:
-    # print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
-    # print("--------------------------------")
-    print(eachObject["name"] + " - "
-          +str(eachObject["percentage_probability"]),
-          eachObject["box_points"])
+    print(eachObject["name"] + " - " + str(eachObject["percentage_probability"]), eachObject["box_points"])
