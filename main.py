d5ec563cfd80594eed35f75129d6e24bb05126e5
Victor <victorp2908@gmail.com>
2023-02-09 23:24:08 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from imageai.Detection import ObjectDetection
 import os
 
+filename = 5
+
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
@@ -12,9 +14,15 @@ detector.setModelTypeAsRetinaNet()  # Самая крупная модель
 detector.setModelPath("models/retinanet_resnet50_fpn_coco-eeacb38b.pth")
 detector.loadModel()
 
-detections = detector.detectObjectsFromImage(input_image=r"images/image.jpg", output_image_path=r"images/imagenew.jpg")
+detections = detector.detectObjectsFromImage(input_image=rf"images/{filename}.jpg", output_image_path=r"images/imagenew.jpg", minimum_percentage_probability=10)
 # detections = detector.detectObjectsFromImage(input_image=r"image.jpg", output_image_path=r"imagenew.jpg", minimum_percentage_probability=50)
 # detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"), minimum_percentage_probability=30)
 
+animals = {}
 for eachObject in detections:
+    if eachObject["name"] in animals:
+        animals[eachObject["name"]] += 1
+    else:
+        animals[eachObject["name"]] = 1
     print(eachObject["name"] + " - " + str(eachObject["percentage_probability"]), eachObject["box_points"])
+print(animals)
